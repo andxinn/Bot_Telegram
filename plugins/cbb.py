63c7d36210988a2e5374e7d565135b3382ab5c25
andxinn <101297390+andxinn@users.noreply.github.com>
2022-03-10 06:37:08 +0700

    Add files via upload

---
+++ plugins/cbb.py
@@ -0,0 +1,25 @@
+# (©)ANDXINN
+
+from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
+
+from bot import Bot
+from config import CHANNEL, GROUP, OWNER
+
+
+@Bot.on_callback_query()
+async def cb_handler(client: Bot, query: CallbackQuery):
+    data = query.data
+    if data == "about":
+        await query.message.edit_text(
+            text=f"<b>Tentang Bot ini:\n\n • Owner: @{OWNER}\n • Channel: @{CHANNEL}\n • Group: @{GROUP}\n • Source Code: <a href='https://github.com/andxinn/Bot_Telegram'>Klik Disini</a></b>\n",
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
+                [[InlineKeyboardButton("• 𝗧𝘂𝘁𝘂𝗽 •", callback_data="close")]]
+            ),
+        )
+    elif data == "close":
+        await query.message.delete()
+        try:
+            await query.message.reply_to_message.delete()
+        except BaseException:
+            pass
